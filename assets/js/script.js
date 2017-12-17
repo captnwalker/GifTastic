38483595709d2ca01f5b5a99a983ac52b807f3fd
Steve Walker <captnwalker@gmail.com>
2017-12-16 22:03:47 -0500

    formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,100 @@
 
-      var gifys = ["Rick and Morty", "Bo Jack Horseman", "South Park", "The Simpsons"];
+var gifys = ["Rick and Morty", "Bo Jack Horseman", "South Park", "The Simpsons"];
 
-      
-      function displayGifInfo(gifName) {
 
-        var gify = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gify + "&api_key=ED5M7CisPjLAtbyt9UiIJpQdgioBnDSz&limit=10";
+function displayGifInfo(gifName) {
 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {   
-          console.log(queryURL);
-          console.log(response);
-          var gifyDiv = $("<div class='gify'>");
-          var rating = response.data.rating;
-          var results = response.data; 
-          for (var i = 0; i < gifys.length; i++) {
-    //>>>>>>>>>>>>>>>>
-          // Creating and storing a div tag
-          var gifyDiv = $("<div>");
-          
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+  var gify = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gify + "&api_key=ED5M7CisPjLAtbyt9UiIJpQdgioBnDSz&limit=10";
 
-          // Creating and storing an image tag
-          var gify = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          gify.attr("src", results[i].images.fixed_height.url);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function (response) {
+    console.log(queryURL);
+    console.log(response);
+    var gifyDiv = $("<div class='gify'>");
+    var rating = response.data.rating;
+    var results = response.data;
+    for (var i = 0; i < gifys.length; i++) {
+      //>>>>>>>>>>>>>>>>
+      // Creating and storing a div tag
+      var gifyDiv = $("<div>");
 
-          // Appending the paragraph and image tag to the animalDiv
-          //gifyDiv.append(p);
-          gifyDiv.append(gify);
+      // Creating a paragraph tag with the result item's rating
+      var p = $("<p>").text("Rating: " + results[i].rating);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifs-appear-here").prepend(gifyDiv);
+      // Creating and storing an image tag
+      var gify = $("<img>");
+      // Setting the src attribute of the image to a property pulled off the result item
+      gify.attr("src", results[i].images.fixed_height.url);
 
-    //<<<<<<<<<<<<<
-          var pRate = $("<p>").text("Rating: " + rating);
+      // Appending the paragraph and image tag to the animalDiv
+      //gifyDiv.append(p);
+      gifyDiv.append(gify);
 
-          gifyDiv.append(pRate);
+      // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+      $("#gifs-appear-here").prepend(gifyDiv);
 
-          // Retrieving the URL for the image
-          var imgURL = response.rating;
+      //<<<<<<<<<<<<<
+      var pRate = $("<p>").text("Rating: " + rating);
 
-          // Creating an element to hold the image
-          var image = $("<img>").attr("src", imgURL);
+      gifyDiv.append(pRate);
 
-          // Appending the image
-          gifyDiv.append(image);
+      // Retrieving the URL for the image
+      var imgURL = response.rating;
 
-          // Putting the entire movie above the previous movies
-          $("#gify-view").prepend(gifyDiv);
-        }
-      });
-  };
+      // Creating an element to hold the image
+      var image = $("<img>").attr("src", imgURL);
 
-      // Function for displaying movie data
-      function renderButtons() {
+      // Appending the image
+      gifyDiv.append(image);
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
+      // Putting the entire movie above the previous movies
+      $("#gify-view").prepend(gifyDiv);
+    }
+  });
+};
 
-        // Looping through the array of movies
-        for (var i = 0; i < gifys.length; i++) {
+// Function for displaying movie data
+function renderButtons() {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie to our button
-          a.addClass("gify");
-          // Adding a data-attribute
-          a.attr("data-name", gifys[i]);
-          // Providing the initial button text
-          a.text(gifys[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
+  // Deleting the movies prior to adding new movies
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
 
-      // This function handles events where a movie button is clicked
-      $("#add-gify").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var gify = $("#gify-input").val().trim();
+  // Looping through the array of movies
+  for (var i = 0; i < gifys.length; i++) {
 
-        // Adding movie from the textbox to our array
-        gifys.push(gify);
+    // Then dynamicaly generating buttons for each movie in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of movie to our button
+    a.addClass("gify");
+    // Adding a data-attribute
+    a.attr("data-name", gifys[i]);
+    // Providing the initial button text
+    a.text(gifys[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
+}
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+// This function handles events where a movie button is clicked
+$("#add-gify").on("click", function (event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var gify = $("#gify-input").val().trim();
 
-      // Adding a click event listener to all elements with a class of "movie"
-     $(document).on("click", ".gify", displayGifInfo);
+  // Adding movie from the textbox to our array
+  gifys.push(gify);
 
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
+});
+
+// Adding a click event listener to all elements with a class of "movie"
+$(document).on("click", ".gify", displayGifInfo);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
